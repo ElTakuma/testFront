f2832ab1a8aaa7ec6d64ef3295dedd9b220bfa1c
ElTakuma <romeotatch@gmail.com>
2022-03-22 14:10:24 +0100

    commit

---
+++ public/js/scripts.js
@@ -0,0 +1,246 @@
+
+let instance = $(".featured_wrapper");
+$.each( instance, function(key, value) {
+  let arrows = $(instance[key]).find(".arrow"),
+      prevArrow = arrows.filter('.arrow-prev'),
+      nextArrow = arrows.filter('.arrow-next'),
+      box = $(instance[key]).find(".r_content"), 
+      x = 0,
+      mx = 0,
+      maxScrollWidth = box[0].scrollWidth - (box[0].clientWidth / 2) - (box.width() / 2);
+
+  $(arrows).on('click', function() {
+    if ($(this).hasClass("arrow-next")) {
+      x = ((box.width() / 2)) + box.scrollLeft() - 10;
+      box.animate({
+        scrollLeft: x,
+      })
+    } else {
+      x = ((box.width() / 2)) - box.scrollLeft() -10;
+      box.animate({
+        scrollLeft: -x,
+      })
+    }
+      
+  });
+    
+  $(box).on({
+    mousemove: function(e) {
+      var mx2 = e.pageX - this.offsetLeft;
+      if(mx) this.scrollLeft = this.sx + mx - mx2;
+    },
+    mousedown: function(e) {
+      this.sx = this.scrollLeft;
+      mx = e.pageX - this.offsetLeft;
+    },
+    scroll: function() {
+      toggleArrows();
+    }
+  });
+
+  $(document).on("mouseup", function(){
+    mx = 0;
+  });
+  
+  function toggleArrows() {
+    if(box.scrollLeft() > maxScrollWidth - 10) {
+        // disable next button when right end has reached 
+        nextArrow.addClass('disabled');
+      } else if(box.scrollLeft() < 10) {
+        // disable prev button when left end has reached 
+        prevArrow.addClass('disabled')
+      } else{
+        // both are enabled
+        nextArrow.removeClass('disabled');
+        prevArrow.removeClass('disabled');
+      }
+  }
+  
+});
+
+let instance_starting_soon = $(".starting_soon_wrapper");
+$.each( instance_starting_soon, function(key, value) {
+  let arrows = $(instance_starting_soon[key]).find(".arrow"),
+      prevArrow = arrows.filter('.arrow-prev'),
+      nextArrow = arrows.filter('.arrow-next'),
+      box = $(instance_starting_soon[key]).find(".r_content"), 
+      x = 0,
+      mx = 0,
+      maxScrollWidth = box[0].scrollWidth - (box[0].clientWidth / 2) - (box.width() / 2);
+
+  $(arrows).on('click', function() {
+    if ($(this).hasClass("arrow-next")) {
+      x = ((box.width() / 2)) + box.scrollLeft() - 10;
+      box.animate({
+        scrollLeft: x,
+      })
+    } else {
+      x = ((box.width() / 2)) - box.scrollLeft() -10;
+      box.animate({
+        scrollLeft: -x,
+      })
+    }
+      
+  });
+    
+  $(box).on({
+    mousemove: function(e) {
+      var mx2 = e.pageX - this.offsetLeft;
+      if(mx) this.scrollLeft = this.sx + mx - mx2;
+    },
+    mousedown: function(e) {
+      this.sx = this.scrollLeft;
+      mx = e.pageX - this.offsetLeft;
+    },
+    scroll: function() {
+      toggleArrows();
+    }
+  });
+
+  $(document).on("mouseup", function(){
+    mx = 0;
+  });
+  
+  function toggleArrows() {
+    if(box.scrollLeft() > maxScrollWidth - 10) {
+        // disable next button when right end has reached 
+        nextArrow.addClass('disabled');
+      } else if(box.scrollLeft() < 10) {
+        // disable prev button when left end has reached 
+        prevArrow.addClass('disabled')
+      } else{
+        // both are enabled
+        nextArrow.removeClass('disabled');
+        prevArrow.removeClass('disabled');
+      }
+  }
+  
+});
+
+
+let instance_best_sellers = $(".starting_soon_wrapper");
+$.each( instance_best_sellers, function(key, value) {
+  let arrows = $(instance_best_sellers[key]).find(".arrow"),
+      prevArrow = arrows.filter('.arrow-prev'),
+      nextArrow = arrows.filter('.arrow-next'),
+      box = $(instance_best_sellers[key]).find(".r_content"), 
+      x = 0,
+      mx = 0,
+      maxScrollWidth = box[0].scrollWidth - (box[0].clientWidth / 2) - (box.width() / 2);
+
+  $(arrows).on('click', function() {
+    if ($(this).hasClass("arrow-next")) {
+      x = ((box.width() / 2)) + box.scrollLeft() - 10;
+      box.animate({
+        scrollLeft: x,
+      })
+    } else {
+      x = ((box.width() / 2)) - box.scrollLeft() -10;
+      box.animate({
+        scrollLeft: -x,
+      })
+    }
+      
+  });
+    
+  $(box).on({
+    mousemove: function(e) {
+      var mx2 = e.pageX - this.offsetLeft;
+      if(mx) this.scrollLeft = this.sx + mx - mx2;
+    },
+    mousedown: function(e) {
+      this.sx = this.scrollLeft;
+      mx = e.pageX - this.offsetLeft;
+    },
+    scroll: function() {
+      toggleArrows();
+    }
+  });
+
+  $(document).on("mouseup", function(){
+    mx = 0;
+  });
+  
+  function toggleArrows() {
+    if(box.scrollLeft() > maxScrollWidth - 10) {
+        // disable next button when right end has reached 
+        nextArrow.addClass('disabled');
+      } else if(box.scrollLeft() < 10) {
+        // disable prev button when left end has reached 
+        prevArrow.addClass('disabled')
+      } else{
+        // both are enabled
+        nextArrow.removeClass('disabled');
+        prevArrow.removeClass('disabled');
+      }
+  }
+  
+});
+
+
+let instance_workshop = $(".workshop_wrapper");
+$.each( instance_workshop, function(key, value) {
+  let arrows = $(instance_workshop[key]).find(".arrow_2"),
+      prevArrow = arrows.filter('.arrow-prev'),
+      nextArrow = arrows.filter('.arrow-next'),
+      box = $(instance_workshop[key]).find(".r_content"), 
+      x = 0,
+      mx = 0,
+      maxScrollWidth = box[0].scrollWidth - (box[0].clientWidth / 2) - (box.width() / 2);
+
+  $(arrows).on('click', function() {
+    if ($(this).hasClass("arrow-next")) {
+      x = ((box.width() / 2)) + box.scrollLeft() - 10;
+      box.animate({
+        scrollLeft: x,
+      })
+    } else {
+      x = ((box.width() / 2)) - box.scrollLeft() -10;
+      box.animate({
+        scrollLeft: -x,
+      })
+    }
+      
+  });
+    
+  $(box).on({
+    mousemove: function(e) {
+      var mx2 = e.pageX - this.offsetLeft;
+      if(mx) this.scrollLeft = this.sx + mx - mx2;
+    },
+    mousedown: function(e) {
+      this.sx = this.scrollLeft;
+      mx = e.pageX - this.offsetLeft;
+    },
+    scroll: function() {
+      toggleArrows();
+    }
+  });
+
+  $(document).on("mouseup", function(){
+    mx = 0;
+  });
+  
+  function toggleArrows() {
+    if(box.scrollLeft() > maxScrollWidth - 10) {
+        // disable next button when right end has reached 
+        nextArrow.addClass('disabled');
+      } else if(box.scrollLeft() < 10) {
+        // disable prev button when left end has reached 
+        prevArrow.addClass('disabled')
+      } else{
+        // both are enabled
+        nextArrow.removeClass('disabled');
+        prevArrow.removeClass('disabled');
+      }
+  }
+  
+});
+
+$('.marquee').marquee({
+  direction: 'left',
+  duration: 15000,
+  startVisible: true,
+  // duplicated:true,
+  delayBeforeStart: 0,
+});
